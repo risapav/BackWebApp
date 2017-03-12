bc97d00fc47f702278c87da045b8ce5df0d21fdd
Pavol Risa <risapav@gmail.com>
2017-03-12 16:21:52 +0100

    initial commit

--- app/controllers/default.js
+++ app/controllers/default.js
@@ -1,13 +1,20 @@
 exports.install = function() {
 	F.route('/', view_logged, ['authorize']);
 	F.route('/', view_homepage);
+        
 	F.route('/', json_login, ['post', '*User']);
+        F.route('/login/', view_login);
 	F.route('/logout/', logout, ['authorize']);
 };
 
 function view_logged() {
 	var self = this;
 	self.plain('You are logged as {0}. To unlogged remove cookie __user or click http://{1}:{2}/logout/'.format(self.user.email, F.ip, F.port));
+}
+
+function view_login() {
+	var self = this;
+	self.view('login', { email: '@' });
 }
 
 function view_homepage() {
